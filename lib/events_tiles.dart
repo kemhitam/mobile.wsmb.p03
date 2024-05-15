dbdeb6d3d47b7cfbdef35483f684f12f99d7817f
kemhitam <ainaqemunifi@gmail.com>
2024-05-15 12:56:14 +0800

    Reapply "untitled"
    This reverts commit 72d493760512a37c8e89ab245f4d9d71e16173da.

---
+++ lib/events_tiles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EventsTiles extends StatelessWidget {
+  final String title;
+  const EventsTiles({super.key, required this.title});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hello Word"),
+    );
+  }
+}
